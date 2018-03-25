9625428a988bae6d88b470a6aebba517a62623ba
Ingmar Drewing <ingmar@drewing.de>
2018-03-25 18:13:15 +0200

    Extract util functions

---
+++ util.go
@@ -0,0 +1,18 @@
+package staticController
+
+import (
+	"fmt"
+	"time"
+)
+
+// Transforms a date to filepath
+func GenerateDatePath() string {
+	now := time.Now()
+	return fmt.Sprintf("%d/%d/%d/", now.Year(), now.Month(), now.Day())
+}
+
+// Generate date and time string
+func GetDate() string {
+	n := time.Now()
+	return fmt.Sprintf("%d-%d-%d %d:%d:%d", n.Year(), n.Month(), n.Day(), n.Hour(), n.Minute(), n.Second())
+}
