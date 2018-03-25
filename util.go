c808fe6d4c08e4d2e9543647c8cd72b56392e48a
Ingmar Drewing <ingmar@drewing.de>
2018-03-25 18:38:39 +0200

    Factor out util method

--- util.go
+++ util.go
@@ -3,6 +3,8 @@ package staticUtil
 import (
 	"fmt"
 	"time"
+
+	"github.com/ingmardrewing/fs"
 )
 
 // Transforms a date to filepath
@@ -15,4 +17,12 @@ func GenerateDatePath() string {
 func GetDate() string {
 	n := time.Now()
 	return fmt.Sprintf("%d-%d-%d %d:%d:%d", n.Year(), n.Month(), n.Day(), n.Hour(), n.Minute(), n.Second())
+}
+
+func DirExists(path string) bool {
+	exits, err := fs.PathExists(path)
+	if err != nil {
+		return false
+	}
+	return len(path) > 0 && exits
 }
